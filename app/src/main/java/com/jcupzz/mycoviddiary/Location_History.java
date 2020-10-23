2b524b896e3527e0b1436a2fe4e3721095921288
Jcupzz <jcupzz255@gmail.com>
2020-10-24 00:50:54 +0530

    added snackbar added network checker

--- app/src/main/java/com/jcupzz/mycoviddiary/Location_History.java
+++ app/src/main/java/com/jcupzz/mycoviddiary/Location_History.java
@@ -2,12 +2,21 @@ package com.jcupzz.mycoviddiary;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.coordinatorlayout.widget.CoordinatorLayout;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 import androidx.recyclerview.widget.RecyclerView.Adapter;
 
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
 import android.content.SharedPreferences;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -15,6 +24,7 @@ import android.widget.Toast;
 import com.airbnb.lottie.LottieAnimationView;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
+import com.google.android.material.snackbar.Snackbar;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
@@ -34,11 +44,16 @@ public class Location_History extends AppCompatActivity {
     ArrayList<Lati_Longi_Models> downModelArrayList = new ArrayList<>();
     SharedPreferences shared;
     LottieAnimationView lottieAnimationView;
+    ConstraintLayout coordinatorLayout;
+    View view;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_location__history);
 
+        coordinatorLayout = findViewById(R.id.cl);
         db = FirebaseFirestore.getInstance();
         lottieAnimationView = findViewById(R.id.l_animation_view);
         l_no_content = findViewById(R.id.l_no_content_id);
@@ -50,26 +65,30 @@ public class Location_History extends AppCompatActivity {
         mRecyclerView.setHasFixedSize(true);
         mRecyclerView.setLayoutManager(new LinearLayoutManager(this));
 
+        if (isInternetAvailable(Location_History.this)) {
+            //do nothing
+        } else {
+            Toasty.warning(Location_History.this, "Please connect to internet!", Toasty.LENGTH_SHORT, true).show();
+        }
+
+
         if (downModelArrayList.size() > 0)
             downModelArrayList.clear();
 
-        Toasty.info(Location_History.this,"Tap on cards to view the location on maps",Toasty.LENGTH_LONG,true).show();
 
         db.collection(email_id)
                 .get()
                 .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
                     @Override
                     public void onComplete(@NonNull Task<QuerySnapshot> task) {
-                        if(!(task.getResult().isEmpty())) {
+                        if (!(task.getResult().isEmpty())) {
 
 
                             for (DocumentSnapshot documentSnapshot : (task.getResult())) {
-                                Lati_Longi_Models lati_longi_models = new Lati_Longi_Models(documentSnapshot.getTimestamp("timestamp"), documentSnapshot.getString("day"), documentSnapshot.getString("date"), documentSnapshot.getString("lati_S"), documentSnapshot.getString("longi_S"),documentSnapshot.getString("address"));
+                                Lati_Longi_Models lati_longi_models = new Lati_Longi_Models(documentSnapshot.getTimestamp("timestamp"), documentSnapshot.getString("day"), documentSnapshot.getString("date"), documentSnapshot.getString("lati_S"), documentSnapshot.getString("longi_S"), documentSnapshot.getString("address"));
                                 downModelArrayList.add(lati_longi_models);
                             }
-                        }
-                        else
-                        {
+                        } else {
                             lottieAnimationView.setVisibility(View.VISIBLE);
                             l_no_content.setVisibility(View.VISIBLE);
                         }
@@ -79,5 +98,51 @@ public class Location_History extends AppCompatActivity {
                 });
 
     }
+
+    public static boolean isInternetAvailable(Context context) {
+        NetworkInfo info = (NetworkInfo) ((ConnectivityManager)
+                context.getSystemService(Context.CONNECTIVITY_SERVICE)).getActiveNetworkInfo();
+
+        if (info == null) {
+            //Log.d(TAG,"no internet connection");
+            return false;
+        } else {
+            if (info.isConnected()) {
+                // Log.d(TAG," internet connection available...");
+                return true;
+            } else {
+                // Log.d(TAG," internet connection");
+                return true;
+            }
+
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.location_history_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+
+        int id = item.getItemId();
+        if (id == R.id.location_history_menu_id) {
+          final Snackbar snackbar =  Snackbar.make(coordinatorLayout, "To track your location we update your location to our database during every 10 minutes.Also don't forget to tap on the cards to view location on the map", Snackbar.LENGTH_SHORT);
+          snackbar.setAction("Ok", new View.OnClickListener() {
+             @Override
+             public void onClick(View v) {
+                 snackbar.dismiss();
+             }
+         });
+          snackbar.show();
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
 
 }
